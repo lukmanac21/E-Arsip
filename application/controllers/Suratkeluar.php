a49a1ff27f4d748e04c3ee3a1379effe177542d2
lukmanac21 <32562123+lukmanac21@users.noreply.github.com>
2020-09-29 09:18:01 +0700

    -

---
+++ application/controllers/Suratkeluar.php
@@ -0,0 +1,96 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Suratkeluar extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('main');
+        $this->load->library('session');
+        if (!$this->session->has_userdata('id_user')) {
+            redirect('Login');
+        }
+    }
+    public function index()
+    {
+        $id_role                = $this->session->userdata('id_role');
+        $data['menu']             = $this->main->get_menu_selected($id_role);
+        $data['surat_masuk']   = $this->main->get_data('mst_surat_masuk');
+
+        $this->load->view('Suratkeluar/index', $data);
+    }
+    public function add_data()
+    {
+        $id_role                = $this->session->userdata('id_role');
+        $data['menu']             = $this->main->get_menu_selected($id_role);
+
+        $this->load->view('Suratkeluar/tambah', $data);
+    }
+    public function save_data()
+    {
+        $data['no_surat']               = $this->input->post('no_surat');
+        $data['tgl_surat']              = $this->input->post('tgl_surat');
+        $data['pengirim_surat']         = $this->input->post('pengirim_surat');
+        $data['perihal_surat']          = $this->input->post('perihal_surat');
+        $data['tgl_terima_surat']       = $this->input->post('tgl_terima_surat');
+        $data['no_agenda_surat']       = $this->input->post('no_agenda_surat');
+        $images                         = "";
+        $upload                         = $this->main->upload_file_surat();
+        if ($upload['result'] == "success") { // Jika proses upload sukses
+            $images = $upload['file']['file_name'];
+        } else { // Jika proses upload gagal
+            var_dump($upload['error']); // Ambil pesan error uploadnya untuk dikirim ke file form dan ditampilkan
+        }
+
+        $data['bukti_surat']         = $images ? $images : '';
+
+        $this->main->insert_data('mst_surat_masuk', $data);
+        redirect('Suratkeluar/index');
+    }
+    public function edit_data($id_surat_masuk)
+    {
+        $id_role                    = $this->session->userdata('id_role');
+        $data['menu']               = $this->main->get_menu_selected($id_role);
+        $where                      = ['id_surat_masuk' => $id_surat_masuk];
+        $data['surat_masuk']        = $this->main->get_data_where('mst_surat_masuk', $where);
+
+        $this->load->view('Suratkeluar/ubah', $data);
+    }
+    public function update_data()
+    {
+        $where['id_surat_masuk']        = $this->input->post('id_surat_masuk');
+        $data['no_surat']               = $this->input->post('no_surat');
+        $data['tgl_surat']              = $this->input->post('tgl_surat');
+        $data['pengirim_surat']         = $this->input->post('pengirim_surat');
+        $data['perihal_surat']          = $this->input->post('perihal_surat');
+        $data['tgl_terima_surat']       = $this->input->post('tgl_terima_surat');
+        $data['no_agenda_surat']       = $this->input->post('no_agenda_surat');
+        $gambarLama                     = $this->input->post('gambarLama');
+
+        $upload                         = $this->main->upload_file_surat();
+        if ($upload['result'] == "success") { // Jika proses upload sukses
+            $images = $upload['file']['file_name'];
+            if (file_exists('./assets/img/Suratkeluar/' . $gambarLama)) {
+                unlink('./assets/img/Suratkeluar/' . $gambarLama);
+            }
+        } else { // Jika proses upload gagal
+            $images = $gambarLama; // Ambil pesan error uploadnya untuk dikirim ke file form dan ditampilkan
+        }
+
+        $data['bukti_surat']         = $images;
+
+        $this->main->update_data('mst_surat_masuk', $data, $where);
+        redirect('Suratkeluar/index');
+    }
+    public function delete_data()
+    {
+        $where['id_surat_masuk']         = $this->input->post('id_surat_masuk');
+        $gambarLama                      = $this->input->post('gambarLama');
+        if (file_exists('./assets/img/suratkeluar/' . $gambarLama)) {
+            unlink('./assets/img/suratmasuk/' . $gambarLama);
+        }
+        $this->main->delete_data('mst_surat_masuk', $where);
+        redirect('Suratkeluar/index');
+    }
+}
--- application/controllers/User.php
+++ application/controllers/User.php
@@ -25,30 +25,28 @@ class user extends CI_Controller {
 	}
 	public function save_data(){
         $data['id_role'] 	    = $this->input->post('id_role');
-        $data['id_dinas'] 	    = $this->input->post('id_dinas');
         $data['nama_user'] 	    = $this->input->post('nama_user');
         $data['email_user'] 	= $this->input->post('email_user');
         $data['password_user'] 	= sha1($this->input->post('password_user',TRUE));
 		
-		$this->main->insert_data('tbl_user',$data);
+		$this->main->insert_data('mst_user',$data);
 		redirect('user/index');
 	}
 	public function edit_data($id_user){
 		$id_role 				= $this->session->userdata('id_role');
         $data['menu'] 			= $this->main->get_menu_selected($id_role);
-        $data['role']           = $this->main->get_data_where('tbl_role','id_role != 1 ');
+        $data['role']           = $this->main->get_data('mst_role');
 		$where 					= ['id_user' => $id_user];
-		$data['user'] 			= $this->main->get_data_where('tbl_user',$where);
-		$this->load->view('user_ubah',$data);
+		$data['user'] 			= $this->main->get_data_where('mst_user',$where);
+		$this->load->view('user/ubah',$data);
 	}
 	public function update_data(){
 		$where['id_user'] 		= $this->input->post('id_user');
         $data['id_role'] 	    = $this->input->post('id_role');
-        $data['id_dinas'] 	    = $this->input->post('id_dinas');
         $data['nama_user'] 	    = $this->input->post('nama_user');
         $data['email_user'] 	= $this->input->post('email_user');
         $data['password_user'] 	= sha1($this->input->post('password_user',TRUE));
-		$this->main->update_data('tbl_user',$data,$where);
+		$this->main->update_data('mst_user',$data,$where);
 		redirect('user/index');
 
 	}
